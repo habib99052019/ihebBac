5891eff80f3cbaa2fd75e1eb70bacb96ae11d264
habib99052019 <84059797+habib99052019@users.noreply.github.com>
2022-12-19 10:14:06 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
 const express = require('express')
 //body parser
 var bodyParser = require('body-parser');
-
-const path = require('path');
-const multer = require('multer');
-
-
-
+var http = require('http');
 console.log('produit')
 const app = express();
 //app.use(bodyParser.urlencoded({ extended: false }))
@@ -15,6 +10,9 @@ const app = express();
 app.use(express.json({ extended: false, limit: '50mb' }))
 app.use(express.urlencoded({ limit: '50mb', extended: false, parameterLimit: 50000 }))
 
+app.get('/backend', (req, res) => {
+    res.send('Hello Backend!');
+});
 const connect = require('./dataBase/connect')
 
 const produitApi=require('./catlogue/routes/produitApi')
@@ -22,10 +20,7 @@ const catigorieApi = require('./catlogue/routes/catigorieApi')
 const sousCatigorieApi=require('./catlogue/routes/sousCatigorieApi')
 const userApi= require('./catlogue/routes/userApi')
 const emailApi= require('./catlogue/routes/emailApi')
-const vila = require('./catlogue/routes/vilaApi')
-const appr= require('./catlogue/routes/appApi')
-const off= require('./catlogue/routes/off-plan')
-const rent= require('./catlogue/routes/rentApi')
+
 //activer les api
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -34,7 +29,6 @@ app.use((req, res, next) => {
     next();
   });
 
-
 //routes
 
 app.use('/backend/produit',produitApi);
@@ -42,57 +36,9 @@ app.use('/backend/catigorie',catigorieApi)
 app.use('/backend/sousCat',sousCatigorieApi);
 app.use('/backend/email',emailApi);
 app.use('/backend/todo',userApi);
-app.use('/backend/off',off);
-app.use('/backend/vila',vila);
-app.use('/backend/appr',appr);
-app.use('/backend/rent',rent);
-
 //port
-const port = process.env.POR || 5900
-//multer
-var name=""
-app.use('/backend/uploads/', express.static(path.join(__dirname, '/uploads')));
-
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, 'uploads');
-        console.log(file);
-    },
-    filename: (req, file, cb) => {
-      name = Date.now() + file.originalname //path.extname(file.originalname);
-        console.log(file);
-        console.log(name);
-        cb(null, name);
-    }
-});
-const fileFilter = (req, file, cb) => {
-    cb(null, true);
-   /* if (file.mimetype == 'image/jpeg' || file.mimetype == 'image/png' || file.mimetype == 'image/pdf') {
-        cb(null, true);
-    } else {
-        cb(null, false);
-    }*/
-}
-const upload = multer({ storage: storage, fileFilter: fileFilter });
-
-//Upload route
-app.post('/upload', upload.single('image'), (req, res, next) => {
-    try {
-        /*return res.status(201).json({
-            message: 'File uploded successfully'
-        });*/
-        return res.status(201).json({
-            message: 'File uploded successfully',
-            source:'https://heart-of-carthage-dubai.com/backend/uploads/'+name,
-            name:name
-        });
-        
-    } catch (error) {
-        console.error(error);
-    }
-});
-//fin multer
-app.listen(port,()=>console.log(`Server listen on the port`,port)) ;
+const port = process.env.PORT || 5900;
+app.listen(port,()=>console.log(`Server listen on the port ${port}`)) ;
 
 //port
 
